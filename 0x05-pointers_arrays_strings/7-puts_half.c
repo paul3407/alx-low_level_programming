2bc1876711a6da70753921f0ddca381fcc053ce3
paul3407 <okechukwup3407@gmail.com>
2023-02-28 07:45:09 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,28 +9,21 @@
 
 void puts_half(char *str)
 {
-	int j = 0;
-	int k;
+	int len = 0;
 
-	while (str[j] != '\0')
+	while (*str != '\0')
 	{
-		j++;
-	}
-
-	if (j % 2 == 1)
-	{
-		k = (j - 1) / 2;
-		k += 1;
-	}
-	else
-	{
-		k = j / 2;
+		len++;
+		str++;
 	}
 
-	for (; k < j; k++)
+	str -= (len / 2);
+	while (*str != '\0')
 	{
-		_putchar(str[k]);
+		_putchar(*str);
+		str++;
 	}
+
 
 	_putchar('\n');
 }
