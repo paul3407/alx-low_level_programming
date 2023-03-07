5b484c17c3508ab7d33e6e4d5daef1183e2c8853
paul3407 <okechukwup3407@gmail.com>
2023-03-07 02:33:18 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,7 +13,7 @@
 void *_memcpy(void *dest, const void *src, size_t n)
 {
 	unsigned int index;
-	unsigned char *destination - dest;
+	unsigned char *destination = dest;
 	const unsigned char *source = src;
 
 	for (index = 0; index < n; index++)
